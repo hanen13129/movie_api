dcfabac3e5cb1ef2f151f2542f4d4d287801234f
hanen13129 <benhassenhanen@gmail.com>
2021-10-26 10:28:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,137 +1,131 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
 const morgan = require ('morgan');
-const uuid = require('uuid');
-const mongoose = require('mongoose');
-const Models = require('./models.js');
-
-const Movies = Models.Movie;
-const Users = Models.User;
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// Logging middleware
-app.use(morgan('common'));
-// For the sending of static files
+const bodyParser = require('body-parser');
 app.use(express.static('public'));
-// Using body-parser
-app.use(bodyParser.json());
+app.use(morgan('common')); 
 
-// An array of objects with  ten movies
-let movies = [
 
-  {
-    title: 'Movie 1',
-    director: 'Director 1',
-    year:2001
-  },
-  {
-    title: 'Movie 2',
-    director: 'Director 2',
-    year:2005
-  },
-  {
-    title: 'Movie 3',
-    director: 'Director 3',
-    year:2007
-  },
-  {
-    title: 'Movie 4',
-    director: 'Director 4',
-    year:2020
-  },
-  {
-    title: ' Movie 5',
-    director: 'Director 5',
-    year:2015
-  },
-  {
-    title: 'Movie 6',
-    director: 'Director 6',
-    year:2002
-  },
-  {
-    title: 'Movie 7',
-    director: 'Director 7',
-    year:2009
 
+
+
+let topMovies = [
+  {
+    title: 'The Shawshank Redemption',
+    genre: ['Prison', 'Drama', 'Crime Fiction'],
+    director: { name: 'Frank Darabot' },
   },
   {
-    title: 'Movie 8',
-    director: 'Director 8',
-    year:2020
+    title: 'The Godfather',
+    genre: ['Mafia', 'Drama', 'Crime Fiction'],
+    director: { name: 'Francis Ford Coppoia' },
   },
   {
-    title: 'Movie 9',
-    director: 'Director 9',
-    year:2021
+    title: 'The Godfather: Part II',
+    genre: ['Mafia', 'Drama', 'Crime Fiction'],
+    director: { name: 'Francis Ford Coppoia' },
   },
-   {
-    title: 'Movie 10',
-    director: 'Director 10',
-    year:2018
+  {
+    title: '12 Angry Men',
+    genre: ['Drama', 'Trial Drama', 'Crime Film'],
+    director: { name: 'Sidney Lumet' },
+  },
+  {
+    title: 'Schindlers List ',
+    genre: ['War', 'History', 'Black and White'],
+    director: { name: 'Steven Spielberg' },
+  },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    genre: ['Novel', 'Fantasy Fiction', 'Adventure Fiction'],
+    director: { name: 'Peter Jackson' },
+  },
+  {
+    title: 'Pulp Fiction',
+    genre: ['Comedy', 'Mafia', 'Crime Film'],
+    director: { name: 'Quentin Tarantino' },
+  },
+  {
+    title: 'The Good, the Bad and the Ugly',
+    genre: ['Spaghetti Western', 'Western', 'Action'],
+    director: { name: 'Sergio Leone' },
+  },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    genre: ['Novel', 'Fantasy Fiction', 'Adventure Fiction'],
+    director: { name: 'Peter Jackson' },
   },
 ];
 
-// res.send('Welcome to my movie database!');
+// Register New Users.
+let newUsers = [''];
 
 // GET requests
+//Get the home page of myFLix App.
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix app!');
+});
+
+// Get Documentation on Request, URL, and Response formats.
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+// Get a list of all movies to the users.
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
 
 app.get('/movies/:title', (req, res) => {
- 
-  res.json(movies.find((movie) =>{
-  return  movie.title === req.params.title
-  }))  
-}); 
-// Add a movie
-app.post('/movies', (req, res) => {
-  let newMovie = req.body;
-
-  if (!newMovie.title) {
-    const message = 'Missing movie title in request body';
-    res.status(400).send(message);
-  } else {
-    newMovie.id = uuid.v4();
-    Movies.push(newMovie);
-    res.status(201).send(newMovie);
-  }
-});
-// Delet a movie from the list by id
-app.delete('/movies/:id', (req, res) => {
-  let movie = Movies.find((movie) => {
-    return movie.id === req.params.id
-  });
-
-  if (movie) {
-    Movies = Movies.filter((obj) => { return obj.id !== req.params.id });
-    res.status(201).send('Movie with the ID of ' + req.params.id + ' was deleted.');
-  } else {
-    res.status(404).send(`Movie with the id ${req.params.id} was not found.`);
-  }
+  res.send(
+    'Successful GET request returning data about a single movies by title to the user.'
+  );
 });
 
-// Update the year of a movie by title
-app.put('/movies/:title/:year', (req, res) => {
-  let movie = Movies.find((movie) => {
-    return movie.title = req.params.title
-  });
-
-  if (movie) {
-    movie.year = parseInt(req.params.year);
-    res.status(201).send(`Movie ${req.params.title} was assigned the year of ${req.params.year}.`);
-  } else {
-    res.status(404).send(`Movie wit the title ${req.params.title} was not found.`);
-  }
+// Return data about a genre (description) by name/title (e.g., “Thriller”).
+app.get('/movies/:title/genre', (req, res) => {
+  res.send('Successful GET request returning data about a genre.');
 });
 
-// Error handler
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+// Return data about a director (bio, birth year, death year) by name.
+app.get('/movies/director/:name', (req, res) => {
+  res.send('Successful GET request returning data about a director.');
 });
+
+// POST Request.
+
+// Allow new users to register.
+app.post('/newUser', (req, res) => {
+  res.send('Successful POST request user was able to register.');
+});
+
+// Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+app.post('/newUser/:id/favorites', (req, res) => {
+  res.send('Successful POST request movie has been added to favorites.');
+});
+
+// PUT Request.
+
+// Allow users to update their user info (username, password, email, date of birth).
+app.put('/newUser/:id/info', (req, res) => {
+  res.send('Successful PUT request user info updated.');
+});
+
+// DELETE Request.
+
+// Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later)
+app.delete('/newUser/:id/favorites', (req, res) => {
+  res.send(
+    'Successful DELETE request movie has been deleted from user list of favorites.'
+  );
+});
+
+// Allow existing users to deregister
+app.delete('/newUser', (req, res) => {
+  res.send('Successful DELETE request existing user has been deregistered.');
+});
+
 // listen for requests
 app.listen(8080, () => {
-  console.log('MovieBase is listening in port 8080.');
+  console.log('Your app is listening on port 8080.');
 });
