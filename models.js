2e91d005184d2bcc8cc2ecf553aada312a51b7d8
hanen13129 <benhassenhanen@gmail.com>
2021-11-08 11:21:16 +0100

    Update models.js

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
-
+//requiring mongoose
 const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt'); 2.10
 
+//create movie schema
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
   Description: {type: String, required: true},
@@ -10,13 +12,27 @@ let movieSchema = mongoose.Schema({
   },
   Director: {
     Name: String,
-    Bio: String
+    Bio: String,
   },
   Actors: [String],
   ImagePath: String,
   Featured: Boolean
 });
+//creating genreSchema
+let genreSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Description: {type: String, required: true}
+});
 
+//creating directorSchema
+let directorSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Bio: {type: String, required: true},
+    Birthdate: {type: Date, required: true},
+    Death: String
+});
+
+//create user schema
 let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
@@ -25,8 +41,19 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
+// userSchema.statics.hashPassword = (password) => { 2.10
+//   return bcrypt.hashSync(password, 10);
+// };
+
+// userSchema.methods.validatePassword = function(password) {
+//   return bcrypt.compareSync(password, this.Password);
+// };2.10
 let Movie = mongoose.model('Movie', movieSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 let User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
