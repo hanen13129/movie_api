6addd5b0703b0b4efa6bf2831ac24453f0261a33
hanen13129 <benhassenhanen@gmail.com>
2021-11-08 12:26:23 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,49 +1,50 @@
-// const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+const jwtSecret = 'your_jwt_secret';
+ // This has to be the same key used in the JWTStrategy
 
-// const jwt = require('jsonwebtoken'),
-//   passport = require('passport');
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
 
-// require('./passport'); // Your local passport file
-
-
-// let generateJWTToken = (user) => {
-//   return jwt.sign(user, jwtSecret, {
-//     subject: user.Username, // This is the username you’re encoding in the JWT
-//     expiresIn: '7d', // This specifies that the token will expire in 7 days
-//     algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
-//   });
-// }
+require('./passport'); // Your local passport file
 
 
-// /* POST login. */
-// module.exports = (router) => {
-//   router.post('/login', (req, res) => {
-//     passport.authenticate('local', { session: false }, (error, user, info) => {
-//       if (error || !user) {
-//         return res.status(400).json({
-//           message: 'Something is not right',
-//           user: user
-//         });
-//       }
-//       req.login(user, { session: false }, (error) => {
-//         if (error) {
-//           res.send(error);
-//         }
-//         let token = generateJWTToken(user.toJSON());
-//         return res.json({ user, token });
-//       });
-//     })(req, res);
-//   });
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // This is the username you’re encoding in the JWT
+    expiresIn: '7d', // This specifies that the token will expire in 7 days
+    algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+  });
+}
 
-//   router.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-//   Movies.find()
-//     .then((movies) => {
-//       res.status(201).json(movies);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       res.status(500).send('Error: ' + error);
-//     });
-// });
-// }
+
+/* POST login. */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+
+  router.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+}
 
