0ce75941ec917f9414b6aaeee5602e779f199368
hanen13129 <benhassenhanen@gmail.com>
2021-11-16 10:51:41 +0100

    Update models.js

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 //requiring mongoose
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt'); 2.10
+const bcrypt = require('bcrypt');
 
 //create movie schema
 let movieSchema = mongoose.Schema({
@@ -41,13 +41,13 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
-// userSchema.statics.hashPassword = (password) => { 2.10
-//   return bcrypt.hashSync(password, 10);
-// };
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
 
-// userSchema.methods.validatePassword = function(password) {
-//   return bcrypt.compareSync(password, this.Password);
-// };2.10
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 let Movie = mongoose.model('Movie', movieSchema);
 let Genre = mongoose.model('Genre', genreSchema);
 let Director = mongoose.model('Director', directorSchema);
